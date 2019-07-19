4e0d1070094e621bade273d0cfe7b3691a58135c
Akuma <avinashk654@github.com>
2019-07-19 09:15:55 +0530

    Spring 2.1.6

---
+++ src/main/java/com/profile/mgm/SpringJwtSecurityApplication2Application.java
@@ -0,0 +1,13 @@
+package com.profile.mgm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringJwtSecurityApplication2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringJwtSecurityApplication2Application.class, args);
+	}
+
+}
